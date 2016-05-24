e60001fc29e55bb25c7152aaa9f22a588dfd5703
ayrat <ayrat.khalimov@gmail.com>
2016-05-24 18:20:25 +0500

    restored -o flag: now running <tool input -o output>

--- src/main.cpp
+++ src/main.cpp
@@ -4,19 +4,22 @@
 
 
 
-void print_usage(){
-    cout << "Run with two arguments: <input_aiger_file> [output_file]" << endl;
+void print_usage_exit(){
+    cout << "Run: <input_aiger_file> -o [output_file]" << endl;
     cout << "([output_file] is optional)" << endl;
+    exit(0);
 }
 
 
 int main (int argc, char *argv[]) {
-    if (argc < 2) {
-        print_usage();
-        exit(0);
-    }
+    if (argc == 1 || !(argc == 4 || argc == 2))
+        print_usage_exit();
+
+    if (argc == 4 && string(argv[2]) != "-o")
+            print_usage_exit();
+
     string input_file_name = argv[1];
-    string output_file_name = (argc == 3) ? argv[2]:"stdout";
+    string output_file_name = (argc == 4) ? argv[3]:"stdout";
 
     Synth synthesizer(input_file_name, output_file_name);
     bool is_realizable = synthesizer.run();
